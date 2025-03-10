48d408c71fe5f4eb0926ec96d297d17bd6b2d617
TamilSelvanKumaravel <tamilselvank09112001@gmail.com>
2025-03-10 13:15:31 +0530

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import {zodResolver} from '@hookform/resolvers/zod'  //this package lib make rea
 import { createIssueSchema } from '@/app/validationSchemas';
 import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 // interface IssueForm {
 //   title:string;
@@ -24,6 +25,7 @@ const  NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   });
   const [error,setError] = useState('');
+  const [isSubmitting,setSubmitting] = useState(false);
   console.log(register('title'));
   return (
     <div className='max-w-xl'>
@@ -34,10 +36,12 @@ const  NewIssuePage = () => {
         className='space-y-3' 
         onSubmit={handleSubmit(async(data)=>{
           try{
+            setSubmitting(true);
             await axios.post('/api/issues',data);
             router.push('/issues');
           } catch (error){
             // console.log(error)
+            setSubmitting(false);
             setError('An unexpected error occurred.');
           }
         })}>
@@ -51,7 +55,7 @@ const  NewIssuePage = () => {
           render={({field})=><SimpleMDE placeholder='Description' {...field}/> /*we can't use register by using spread operator way ,so we use contoller */}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
       </form>
     </div>
   )
